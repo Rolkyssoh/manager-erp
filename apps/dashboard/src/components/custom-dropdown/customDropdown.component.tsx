ef5b57f6b25fbc280afa90128b2bc5771e0a4f7b
Rolkyssoh <juventussoh@gmail.com>
2022-03-09 12:12:46 +0100

    Edit custom-dropdown-component and _dropdown-scss

--- apps/dashboard/src/components/custom-dropdown/customDropdown.component.tsx
+++ apps/dashboard/src/components/custom-dropdown/customDropdown.component.tsx
@@ -1,42 +1,58 @@
-import { ActionButton, Text } from '@fluentui/react';
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router';
+// import React, { useEffect, useState } from 'react';
+// import { useLocation } from 'react-router';
 import { useAuthStore } from '../../stores';
+import * as React from 'react';
+import {
+  Callout,
+  Link,
+  mergeStyleSets,
+  Text,
+  FontWeights,
+  ActionButton,
+} from '@fluentui/react';
+import { useBoolean, useId } from '@fluentui/react-hooks';
+import { DefaultButton } from '@fluentui/react/lib/Button';
 
 export interface IDropDwonProps {
   default_props?: boolean;
 }
 
 export const CustomDropdownComponent: React.FC<IDropDwonProps> = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [open, setOpen] = useState<boolean>(false);
-  const [timeOutId, setTimeOutId] = useState<any>(null);
+  const [isCalloutVisible, { toggle: toggleIsCalloutVisible }] =
+    useBoolean(false);
+  const buttonId = useId('callout-button');
+  const labelId = useId('callout-label');
+  const descriptionId = useId('callout-description');
 
   const { updateToken } = useAuthStore();
-  // const {user} = state;
 
   const doLogout = () => {
-    setOpen(false);
     localStorage.clear();
     updateToken('');
   };
 
   return (
-    <div>
-      {/* <button
-        onClick={() => setOpen(!open)}
-        aria-haspopup="true"
-        aria-expanded={isOpen}
-      >
-        Select an option
-      </button> */}
-      <ActionButton text="Profile" onClick={() => setOpen(!open)} />
-      {open && (
-        <div className="dropdown-menu">
-          <div className="dropdown-header">
-            <Text variant="xLarge">Hello, Name</Text>
-          </div>
-          <hr />
+    <>
+      <DefaultButton
+        id={buttonId}
+        onClick={toggleIsCalloutVisible}
+        text={isCalloutVisible ? 'Hide callout' : 'Show callout'}
+        className={styles.button}
+      />
+      {isCalloutVisible && (
+        <Callout
+          className={styles.callout}
+          ariaLabelledBy={labelId}
+          ariaDescribedBy={descriptionId}
+          gapSpace={0}
+          target={`#${buttonId}`}
+          onDismiss={toggleIsCalloutVisible}
+          setInitialFocus
+        >
+          <Text block variant="xLarge" className="dropdown-header" id={labelId}>
+            Callout title here
+          </Text>
+          <hr className="someClass" />
           <div className="dropdowninfos">
             <div className="dropdowninfos_details">
               <Text>First name</Text>
@@ -60,8 +76,27 @@ export const CustomDropdownComponent: React.FC<IDropDwonProps> = () => {
               onClick={doLogout}
             />
           </div>
-        </div>
+        </Callout>
       )}
-    </div>
+    </>
   );
 };
+
+const styles = mergeStyleSets({
+  button: {
+    width: 130,
+  },
+  callout: {
+    width: 320,
+    maxWidth: '90%',
+    // padding: '20px 24px',
+  },
+  title: {
+    marginBottom: 12,
+    fontWeight: FontWeights.semilight,
+  },
+  link: {
+    display: 'block',
+    marginTop: 20,
+  },
+});
